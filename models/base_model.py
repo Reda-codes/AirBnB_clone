c926df2e930405e9a1aeb2369b678ae1c94c7120
Reda <muhammadredabh@gmail.com>
2022-10-28 21:35:03 +0100

    arg support

--- models/base_model.py
+++ models/base_model.py
@@ -3,25 +3,38 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """base class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initializing a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+
     def __str__(self):
-        return '[BaseModel] (' + str(self.id ) + ')'+ ' ' + str(self.__dict__)
+        """method returns the string representation of the object"""
+        return '[BaseModel] (' + str(self.id) + ')' + ' ' + str(self.__dict__)
 
     def save(self):
+        """Updates updated_at to a new time"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """returns a dictionary containing all keys/values"""
         newDict = {}
         newDict.update(self.__dict__)
         newDict.update({'__class__':
-            (str(type(self)).split('.')[-1]).split('\'')[0]})
+                        (str(type(self)).split('.')[-1]).split('\'')[0]})
+
         newDict['created_at'] = self.created_at.isoformat()
         newDict['updated_at'] = self.updated_at.isoformat()
         return newDict
